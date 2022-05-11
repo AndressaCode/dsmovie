af8b913b27cba123fccb83640a5d59926532e763
Andressa Silva <andressaszx@gmail.com>
2022-05-11 20:26:46 -0300

    Save score

--- backend/src/main/java/com/dsmovie/dsmovie/entities/Movie.java
+++ backend/src/main/java/com/dsmovie/dsmovie/entities/Movie.java
@@ -1,9 +1,13 @@
 package com.dsmovie.dsmovie.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -17,9 +21,14 @@ public class Movie {
 	private Double score;
 	private Integer count;
 	private String image;
+	
+	// A coleção set é uma interface, e a classe que implementa essa interface é a HashSet
+	// Não posso instanciar uma interface
+	
+	@OneToMany(mappedBy = "id.movie")
+	private Set<Score> scores = new HashSet<>();
 
 	public Movie() {
-
 	}
 
 	public Movie(Long id, String title, Double score, Integer count, String image) {
@@ -68,6 +77,10 @@ public class Movie {
 
 	public void setImage(String image) {
 		this.image = image;
+	}
+	
+	public Set<Score> getScores(){
+		return scores;
 	}
 
 }
